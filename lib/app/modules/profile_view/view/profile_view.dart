6cfa9a77a270d29d97168b9b2079659c43169e23
Syed Jamal <syedjamal969@gmail.com>
2024-08-22 13:14:15 +0500

    sign in screen

--- lib/app/modules/profile_view/view/profile_view.dart
+++ lib/app/modules/profile_view/view/profile_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:slide_maker/app/modules/profile_view/controller/profile_view_controller.dart';
 import 'package:slide_maker/app/utills/colors.dart';
 import 'package:slide_maker/app/utills/images.dart';
 import 'package:slide_maker/app/utills/size_config.dart';
 
-class ProfileView extends StatelessWidget {
+class ProfileView extends GetView<ProfileViewCTL> {
   const ProfileView({super.key});
 
   @override
@@ -18,7 +20,7 @@ class ProfileView extends StatelessWidget {
               Container(
                 height: SizeConfig.blockSizeVertical * 30,
                 width: SizeConfig.screenWidth,
-                color: AppColors.Bright_Pink_color,
+                // color: AppColors.Bright_Pink_color,
                 child: SvgPicture.asset(
                   AppImages.profile_background,
                   alignment: Alignment.centerLeft,
@@ -39,7 +41,7 @@ class ProfileView extends StatelessWidget {
                 ),
               ),
               Positioned(
-                top: SizeConfig.blockSizeVertical * 8,
+                top: SizeConfig.blockSizeVertical * 7,
                 left: SizeConfig.blockSizeHorizontal * 36,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -64,10 +66,122 @@ class ProfileView extends StatelessWidget {
                     ),
                   ],
                 ),
+              ),
+              Positioned(
+                top: SizeConfig.blockSizeVertical * 22,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: SizeConfig.blockSizeHorizontal * 10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        followers_method("Followers : 0"),
+                        horizontalSpace(SizeConfig.blockSizeHorizontal * 10),
+                        followers_method("Following : 100")
+                      ]),
+                ),
               )
             ],
+          ),
+          Container(
+            height: SizeConfig.blockSizeVertical * 7,
+            width: SizeConfig.screenWidth,
+            decoration: BoxDecoration(color: AppColors.mainColor),
+            child: Center(
+              child: Text(
+                "Your Creations",
+                style: GoogleFonts.inter(
+                    textStyle: TextStyle(
+                        fontSize: SizeConfig.blockSizeHorizontal * 6,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textfieldcolor)),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.blockSizeHorizontal * 4,
+                  vertical: SizeConfig.blockSizeVertical * 1),
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: SizeConfig.blockSizeHorizontal * 4),
+                  height: SizeConfig.blockSizeVertical * 10,
+                  width: SizeConfig.blockSizeHorizontal * 60,
+                  decoration: BoxDecoration(
+                      color: AppColors.textfieldcolor,
+                      borderRadius: BorderRadius.circular(
+                          SizeConfig.blockSizeHorizontal * 7)),
+                  child: Row(
+                    children: [
+                      Container(
+                          height: SizeConfig.blockSizeVertical * 7,
+                          width: SizeConfig.blockSizeHorizontal * 25,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                  SizeConfig.blockSizeHorizontal * 3)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(
+                                SizeConfig.blockSizeHorizontal * 3),
+                            child: Image.asset(
+                              AppImages.slidy_style8[0],
+                              fit: BoxFit.cover,
+                            ),
+                          )),
+                      horizontalSpace(SizeConfig.blockSizeHorizontal * 4),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "Solar Flair",
+                            style: GoogleFonts.inter(
+                                textStyle: TextStyle(
+                                    fontSize:
+                                        SizeConfig.blockSizeHorizontal * 4,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.titles)),
+                          ),
+                          Text(
+                            "Date/Time",
+                            style: GoogleFonts.inter(
+                                textStyle: TextStyle(
+                                    fontSize:
+                                        SizeConfig.blockSizeHorizontal * 3,
+                                    color: AppColors.titles)),
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                      GestureDetector(
+                          onTap: () {}, child: Icon(Icons.more_vert))
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
+      ),
+    );
+  }
+
+  Container followers_method(String text) {
+    return Container(
+      height: SizeConfig.blockSizeVertical * 5,
+      width: SizeConfig.blockSizeHorizontal * 37,
+      decoration: BoxDecoration(
+          color: AppColors.mainColor,
+          borderRadius:
+              BorderRadius.circular(SizeConfig.blockSizeHorizontal * 4)),
+      child: Center(
+        child: Text(
+          text,
+          style: GoogleFonts.inter(
+              textStyle: TextStyle(
+                  fontSize: SizeConfig.blockSizeHorizontal * 4,
+                  color: AppColors.textfieldcolor)),
+        ),
       ),
     );
   }
